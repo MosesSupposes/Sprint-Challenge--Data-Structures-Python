236a537ed0e25f69e3b9d55c1981b330b8644881
Moses A. Samuel <mosamuel98@gmail.com>
2020-04-24 13:43:59 -0700

    get the names algorithm to run in ~1.5 seconds

--- names/names.py
+++ names/names.py
@@ -16,19 +16,11 @@ duplicates = []  # Return the list of duplicates in this data structure
 # Replace the nested for loops below with your improvements
 # -------------- Time complexity: O(n^2) -----------------
 
-# for name_1 in names_1:
-#     for name_2 in names_2:
-#         if name_1 == name_2:
-#             duplicates.append(name_1)
-
-bst = BinarySearchTree("a")
-names_1.sort()
-for name in names_1:
-    bst.iterative_insert(name)
 
 for name in names_2:
-    if bst.iterative_contains(name):
+    if name in names_1:
         duplicates.append(name)
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
